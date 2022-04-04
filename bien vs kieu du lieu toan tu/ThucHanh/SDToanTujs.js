64577dc7898557283dfd239644a13b4d36e544a1
hungnv612 <hungdaubasg@gmail.com>
2022-04-04 14:57:48 +0700

    update

---
+++ bien vs kieu du lieu toan tu/ThucHanh/SDToanTujs.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Nhập chiều dài");
+inputHeight = prompt("Nhập chiều rộng");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("Diện tích Chử nhật là : " + area);
